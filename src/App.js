c73f3e77905debabaef8186cebdc8b10ba800297
iSued <59610863+iSued@users.noreply.github.com>
2021-02-10 14:02:53 +0100

    first update

---
+++ src/App.js
@@ -0,0 +1,146 @@
+/* eslint-disable no-restricted-globals */
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import io from "socket.io-client";
+import {
+  Modal,
+  InputGroup,
+  FormControl,
+  Button,
+  Col,
+  Row,
+} from "react-bootstrap";
+
+const connOpt = {
+  transports: ["websocket", "polling"],
+};
+
+let socket = io("https://striveschool-api.herokuapp.com", connOpt);
+
+function App() {
+  const [receiver, setReceiver] = useState("");
+  const [list, setList] = useState([null]);
+  const [username, setUsername] = useState(null);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [showModal, setShowModal] = useState(true);
+  useEffect(() => {
+    const handleFetch = async () => {
+      let response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/messages/${username}`
+      );
+      let data = await response.json();
+      console.log(data);
+    };
+    handleFetch();
+  }, [username]);
+
+  useEffect(() => {
+    socket.on("connect", () => console.log("connected to socket"));
+    socket.on("list", (list) => {
+      setList([...list]);
+    });
+    socket.on("chatmessage", (msg) => {
+      setMessages((messages) => messages.concat(msg));
+    });
+    console.log("this are the fucking messages", messages);
+
+    socket.on("connect", () => console.log("connected to socket"));
+
+    return () => socket.removeAllListeners();
+  }, [messages]);
+
+  const handleMessage = (e) => {
+    setMessage(e.currentTarget.value);
+  };
+  console.log("this is the receiver", receiver);
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message !== "") {
+      socket.emit("chatmessage", {
+        to: receiver,
+        text: message,
+      });
+
+      setMessage("");
+    }
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+    socket.emit("setUsername", { username });
+  };
+
+  return (
+    <>
+      <Row>
+        <Col className="col-3">
+          <ul>
+            {list
+              .filter((user) => {
+                return user !== username;
+              })
+              .map((user, i) => (
+                <li key={i} onClick={(e) => setReceiver(e.target.innerText)}>
+                  <strong>{user}</strong>
+                </li>
+              ))}
+          </ul>
+        </Col>
+        <Col className="col-9">
+          <h1>{receiver}</h1>
+          <div className="App">
+            <ul id="messages">
+              {messages
+                .filter((user) => {
+                  return user.from == receiver;
+                })
+                .map((msg, i) => (
+                  <li
+                    key={i}
+                    className={msg.from === username ? "ownMessage" : "message"}
+                  >
+                    <strong>{msg.from}</strong> {msg.msg}
+                  </li>
+                ))}
+            </ul>
+            <form id="chat" onSubmit={sendMessage}>
+              <input
+                autoComplete="off"
+                value={message}
+                onChange={handleMessage}
+              />
+              <Button type="submit" className="rounded-0">
+                Send
+              </Button>
+            </form>
+          </div>
+          <Modal
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            show={showModal}
+            onHide={toggleModal}
+          >
+            <Modal.Header>
+              <Modal.Title>Set username</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <InputGroup className="mb-3">
+                <FormControl
+                  onChange={(e) => setUsername(e.currentTarget.value)}
+                ></FormControl>
+              </InputGroup>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={toggleModal}>Submit</Button>
+            </Modal.Footer>
+          </Modal>
+        </Col>
+      </Row>
+    </>
+  );
+}
+
+export default App;
